d7af7be0ff3f55d26d1c89d071fb79223da02895
zhadream <43086874+zhadream@users.noreply.github.com>
2020-05-11 18:00:12 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+let temp = "123"
+document.querySelector("h1").onclick = function() {
+    let name = prompt("input message: ")
+    alert(name)
+}
